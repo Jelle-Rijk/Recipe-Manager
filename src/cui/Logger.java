47251339c023ca1d5b0acca06aed47b71092ceab
Jelle-Rijk <jellerijk@gmail.com>
2025-06-22 15:02:37 +0200

    Log messages now start on a new line in the console.

--- src/cui/Logger.java
+++ src/cui/Logger.java
@@ -4,19 +4,19 @@ import java.time.LocalTime;
 
 public abstract class Logger {
 	public static void log(String message, Object source) {
-		System.out.printf("[%s - %s]: %s", source.getClass().getSimpleName(), LocalTime.now(), message);
+		System.out.printf("[%s - %s]: %s%n", source.getClass().getSimpleName(), LocalTime.now(), message);
 	}
 
 	public static void log(String message) {
-		System.out.printf("[%s]: %s", LocalTime.now(), message);
+		System.out.printf("[%s]: %s%n", LocalTime.now(), message);
 	}
 
 	public static void logError(String message, Object source) {
-		System.err.printf("[%s - %s]: %s", source.getClass().getSimpleName(), LocalTime.now(), message);
+		System.err.printf("[%s - %s]: %s%n", source.getClass().getSimpleName(), LocalTime.now(), message);
 	}
 
 	public static void logError(String message) {
-		System.err.printf("[%s]: %s", LocalTime.now(), message);
+		System.err.printf("[%s]: %s%n", LocalTime.now(), message);
 	}
 
 }
